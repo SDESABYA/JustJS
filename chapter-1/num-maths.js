993bccfc54f13df47408759af938f607a7b314dc
SDESABYA <sdesabya@gmail.com>
2024-10-03 11:00:30 +0000

    good job nigga with numbers and maths

---
+++ chapter-1/num-maths.js
@@ -0,0 +1,28 @@
+console.log('aYO NIGGA WELCOME');
+
+// const num = new Number(45.67);
+// //console.log(num.toPrecision(4));
+
+// const dawg = new Number(100)
+
+// console.log(dawg.toString().length);
+
+// let NIGGA = Math.sqrt(25)
+
+// console.log(NIGGA);
+
+
+// console.log(Math.floor(Math.random()*10)) + 1;   // this gives values btw 0-1
+
+//getting a number a given range 
+
+// min - 1
+// max -6   , just as in dice game
+// make a random dice game
+let max =100;
+let min =50;
+console.log(Math.floor(Math.random()*(max-min))+ min); // this gives number btw min and max
+
+
+
+
